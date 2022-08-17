41da18c4bfa52b36d0507a90aa60164fba00a1d8
Mahika <mahikasharma0007@gmail.com>
2022-08-17 14:06:25 +0530

    link update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextArea from './components/TextArea';
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import {
   BrowserRouter,
   Routes,
@@ -12,46 +12,47 @@ import About from './components/About';
 import Alert from './components/Alert';
 
 function App() {
-  const[mode,setMode]=useState('light');
-  const[alert,setalert]=useState(null);
-  const toggleMode=()=>{
-    if(mode==='light')
-    {
+  const [mode, setMode] = useState('light');
+  const [alert, setalert] = useState(null);
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor='black';
-      showAlert("Dark Mode Is Enabled","light")
+      document.body.style.backgroundColor = 'black';
+      showAlert("Dark Mode Is Enabled", "light")
     }
-    else{
+    else {
       setMode('light')
-      document.body.style.backgroundColor='#f5e6e6';
-      showAlert("Light Mode Is Enabled","info")
+      document.body.style.backgroundColor = '#f5e6e6';
+      showAlert("Light Mode Is Enabled", "info")
     }
   }
-  const showAlert=(message,type)=>{
+  const showAlert = (message, type) => {
     setalert({
-      msg : message,
-      type :type
+      msg: message,
+      type: type
     })
     setTimeout(() => {
       setalert(null)
     }, 1500);
   }
   return (
-  <>
-  <BrowserRouter>
-  <Navbar mode={mode} toggle={toggleMode}/>
-  <Alert alert={alert} />
-  <Routes>
-          <Route exact path="/about" element={<About mode={mode}/>}>
-            
+    <>
+      <BrowserRouter>
+        <Navbar mode={mode} toggle={toggleMode} />
+        <Alert alert={alert} />
+        <Routes>
+          <Route exact path="/about" element={<About mode={mode} />}>
+
           </Route>
-  
-          
-      
-    </Routes>
-    <TextArea mode={mode} showAlert={showAlert}/>
-  </BrowserRouter>
-  </>
+
+          <Route index element={<TextArea mode={mode} showAlert={showAlert} />}>
+
+          </Route>
+
+        </Routes>
+
+      </BrowserRouter>
+    </>
   );
 }
 
