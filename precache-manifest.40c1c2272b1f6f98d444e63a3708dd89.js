2508ead80e686578bbb6062e8eec20b83b1519fd
Mahika <mahikasharma0007@gmail.com>
2023-01-24 00:47:41 +0530

    Updates

---
+++ precache-manifest.40c1c2272b1f6f98d444e63a3708dd89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a1baacd8652027ae529",
+    "url": "/Wordle/static/css/main.a78cde98.chunk.css"
+  },
+  {
+    "revision": "2a1baacd8652027ae529",
+    "url": "/Wordle/static/js/main.2a1baacd.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Wordle/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Wordle/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "267682479aa49bbc375b",
+    "url": "/Wordle/static/js/runtime~main.26768247.js"
+  },
+  {
+    "revision": "c16912600511c0e702f78bb3d88beaa1",
+    "url": "/Wordle/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e1cf92130fcf430e9ee1ef22a1ecfe4a.js"
+  "/Wordle/precache-manifest.40c1c2272b1f6f98d444e63a3708dd89.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Wordle/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
