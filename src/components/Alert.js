c31f19cac0b6d6c820e5ed9d9bc4a3c19aa4562f
Mahika <mahikasharma0007@gmail.com>
2023-01-24 01:12:06 +0530

    update

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Alert(props) {
     return (
         <>
-            <div style={{ height: '5em' }}>
+            <div style={{ height: '4em' }}>
                 {props.alert &&
                     <div className={`alert alert-${props.alert.type}`} role="alert" style={{ textAlign: 'center' }}><strong>
                         {props.alert.msg}
